265bfecbb0cf34b224ac640a04c5f9ab9ab16295
Mikolaj Bojdol <mikoajbojdo@gmail.com>
2025-07-12 20:14:38 +0200

    Added Hud element - health bar also scripts for Hearts system

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Health : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+
+public class Health : MonoBehaviour {
+    public int maxHealth = 100;
+    public int health = 0;
+    public HealthBar healthBar;
+
+    /* Dla serduszek ten system bêdzie musia³ wygl¹daæ inaczej
+    public float health, maxHealth;
+    */
+
+    void Start() {
+        health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            TakeDamage(20);
+
+        }
+    }
+
+    void TakeDamage(int damage) {
+        health -= damage;
+        healthBar.SetHealth(health);
     }
 }
